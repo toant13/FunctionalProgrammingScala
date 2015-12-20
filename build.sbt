c57ac75a057db6863bebdb474f2153649750c351
Tony Tran <toant713@gmail.com>
2015-12-20 13:41:40 -0500

    Adding test class library dependencies

--- build.sbt
+++ build.sbt
@@ -3,4 +3,15 @@ name := "ScaleFunctionalProject"
 version := "1.0"
 
 scalaVersion := "2.11.7"
-    
+
+
+libraryDependencies ++= Seq(
+  "junit" % "junit" % "4.12" % "test"
+)
+
+libraryDependencies += "com.novocode" % "junit-interface" % "0.11"
+
+
+libraryDependencies ++= Seq(
+  "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
+)
---
+++ src/test/scala/com/functional/AssignmentTwoTest.scala
@@ -0,0 +1,21 @@
+package com.functional
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/**
+  * Created by toantran on 12/20/15.
+  */
+@RunWith(classOf[JUnitRunner])
+class AssignmentTwoTest extends FunSuite {
+
+
+  import com.functional.course.AssignmentTwo._
+
+  test("singletonSet with 3") {
+//    val test = singletonSet(3)
+    assert(true == true)
+  }
+}
